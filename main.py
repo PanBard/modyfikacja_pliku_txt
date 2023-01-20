11e40a5385b61254a83555476d22e638d23197bd
PanBard <93971955+PanBard@users.noreply.github.com>
2023-01-20 22:52:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import codecs
+
+
+nazwa_pliku_pierwotnego = "drillznumerkami.txt"
+
+nazwa_nowego_pliku = "nowyplikdrill.txt"
+
+
+
+plik_nowy = codecs.open(nazwa_nowego_pliku,'a',"utf-8")
+
+
+with codecs.open(nazwa_pliku_pierwotnego,'r',"utf-8") as file:
+    lines = list(file)
+    licznik = 1
+    modyfikowane_zdanie = ""
+    numer_z_pliku =""
+
+    for sentence in lines:
+        # print(sentence.find('[#'))
+        modyfikowane_zdanie = ""
+        if sentence.find('[#') == 0:
+            for x in range(len(sentence)) :
+                if (sentence[x] == '[') and (sentence[x+1]=='#'):
+                    for z in range(len(sentence)):
+                        if sentence[x+z].isnumeric():
+                            numer_z_pliku += sentence[x+z]
+                        if sentence[x+z] == ']':
+                            break
+
+
+            for x in sentence:
+                modyfikowane_zdanie += x
+                if x == ']':
+                    modyfikowane_zdanie += " "+numer_z_pliku
+                    numer_z_pliku = ''
+            plik_nowy.write(modyfikowane_zdanie)
+        else: plik_nowy.write(sentence)
+
+        #     for x in range(sentence) :
+        #         if sentence.find(']') == x
+        #     print(sentence)
+        # else:
+        #     plik_nowy.write("")
+        #     licznik +=1
+        # if sentence.find(">>") == 0:
+        #     plik_nowy.write("["+str(licznik)+"]"+sentence)
+
+
+plik_nowy.close()
